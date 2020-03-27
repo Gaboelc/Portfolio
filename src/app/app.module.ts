e0ea528139fdaf4c837b1ff397a595374a2fd778
unknown <gabrielleon917@gmail.com>
2020-03-26 22:05:25 -0600

    About me section complete     to do: add styles
    added angular materials

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// Own
+import {MaterialModule} from './material/material.module';
 import { TypeAnimationComponent } from './type-animation/type-animation.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TypeAnimationComponent
+    TypeAnimationComponent,
+    AboutMeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
