4879e5135cea05d19616e976aee82792006219b8
unknown <gabrielleon917@gmail.com>
2020-04-11 03:23:42 -0600

    Added Timeline to the body of the web page, 	To do: 			*Add more information about studies and jobs. 				*List things that i did in my intership and what i learned in my specialization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 // Own
-import {MaterialModule} from './material/material.module';
-import { TypeAnimationComponent } from './type-animation/type-animation.component';
-import { AboutMeComponent } from './about-me/about-me.component';
+import { MaterialModule } from "./material/material.module";
+import { TypeAnimationComponent } from "./type-animation/type-animation.component";
+import { AboutMeComponent } from "./about-me/about-me.component";
+import { TimelineComponent } from "./timeline/timeline.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     TypeAnimationComponent,
     AboutMeComponent,
+    TimelineComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
